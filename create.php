1e005eaf939d7433bd7585c8865cb59daf7ec290
Thomas <sora1000124@gmail.com>
2021-01-28 19:13:37 -0600

    start of creating html page with php

---
+++ create.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <?php
+  function getTitle(){
+    echo "<title>Lebrun - Self-portrait in a Straw Hat</title>";
+  }?>
+</head>
+  <body>
+    <?php echo "<h1>Lebrun straw hat</h1>"?>
+  </body>
+  </html>
