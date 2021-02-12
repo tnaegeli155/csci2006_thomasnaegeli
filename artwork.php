47c3a2c48b44aec2a9337f3a7e2f6cd7f2633ca1
Thomas <sora1000124@gmail.com>
2021-02-12 17:35:34 -0600

    Changed primary file from create.php to artwork.php

--- artwork.php
+++ artwork.php
@@ -2,13 +2,12 @@
 function getTitle(){
   echo "<title>Lebrun - Self-portrait in a Straw Hat</title>";
 }
-
 function getBody(){
   echo '<body>
       <header>
           <nav class="user">
               <ul>
-                  <li><a href="#">My Account</a></li>
+                  <li><a href="http://localhost/artwork.php?q=myAccount">My Account</a></li>
                   <li><a href="#">Wish List</a></li>
                   <li><a href="#">Shopping Cart</a></li>
               </ul>
@@ -16,7 +15,7 @@ function getBody(){
           <h1>Art Store</h1>
           <nav>
               <ul>
-                  <li><a href="#">Home</a></li>
+                  <li><a href="http://localhost/artwork.php?q=Home">Home</a></li>
                   <li><a href="#">About Us</a></li>
                   <li><a href="#">Art Works</a></li>
                   <li><a href="#">Artists</a></li>
@@ -115,33 +114,73 @@ function getBody(){
 
   </body>';
 }
-
-
-?>
-
-<?php
 function accountDetails(){
-  return<<<form>
-  <label>Name: </label>
-  <input type="text" name="Name" Value="Name"><br>
-  <label>Username: </label>
-  <input type="text" name="Username" value="Username"><br>
-  <label>Password: </label>
-  <input type="text" name="Password" value="Password"><br>
-  <label>Address</label>
-  <input type="text" name="address1" value="Address"><br>
-  <label>Address 2</label>
-  <input type="text" name="address2" value="Address2"><br>
-  
+  echo '<body>
+      <header>
+          <nav class="user">
+              <ul>
+                  <li><a href="http://localhost/account.php?q=myAccount">My Account</a></li>
+                  <li><a href="#">Wish List</a></li>
+                  <li><a href="#">Shopping Cart</a></li>
+              </ul>
+          </nav>
+          <h1>Art Store</h1>
+          <nav>
+              <ul>
+                  <li><a href="http://localhost/artwork.php?q=Home">Home</a></li>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Art Works</a></li>
+                  <li><a href="#">Artists</a></li>
+              </ul>
+          </nav>
+      </header>';
+  echo '<form action="/artwork.php" method="post">';
+  echo '<label>Name: </label>';
+  echo '<input type="text" name="Name" value="Name"><br>';
+  echo '<label>Username: </label>';
+  echo '<input type="text" name="Username" value="Username"><br>';
+  echo '<label>Password: </label>';
+  echo '<input type="text" name="Password" value="Password"><br>';
+  echo '<label>Address: </label>';
+  echo '<input type="text" name="address1" value="Address"><br>';
+  echo '<input type="text" name="address2" value="Apt or Unit"><br>';
+  echo '<label>City: </label>';
+  echo '<input type="text" name="City" value="City"><br>';
+  echo '<label>State: </label>';
+  echo '<input type="text" name="state" value="State"><br>';
+  echo '<label>ZIP: </label>';
+  echo '<input type="text" name="Zip" value="Zip"><br>';
+  echo '<input type="submit" name="Save Changes" value="Save Changes">';
+  echo '</form>';
+  echo '<footer>
+      <p>All images are copyright to their owners. This is just a hypothetical site ©2020 Copyright Art Store</p>
+  </footer>
+</body>';
 }
 ?>
 <!DOCTYPE html>
+<?php
+if(!empty($_POST)){
+  echo '<script>alert("Changes Saved")</script>';
+}
+$check = isset($_GET['q']);
+if($check!=true||$check!=false){
+}
+?>
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
   <?php getTitle()?>
 
 </head>
-  <?php getBody()?>
+  <?php
+  if($check==false){
+    getBody();
+  }else{
+  $query = $_GET['q'];
+  if($query=="myAccount"){
+    accountDetails();
+  }elseif($query=="Home"){
+    getBody();
+  }}?>
 </html>
